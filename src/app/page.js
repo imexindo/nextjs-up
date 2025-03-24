6aa4ab9dbd18bfa9d972069f6147c6944368257a
devImexindo <dev.imexindo@gmail.com>
2025-03-24 15:45:26 +0700

    fidx

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="index-page">
-      <h1>Deployment</h1>
+      <h1>NEXTJS</h1>
+      <a href="/about" style={{ textDecoration: 'none', color: 'blue' }}>Tentang Kami</a>
     </div>
   );
 }
